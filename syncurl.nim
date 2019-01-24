dd91266ad8e871bf9dd31ccd02919c22fbe71145
Ivan <ivan.grubisic.mail@gmail.com>
2019-01-24 21:06:07 +0100

    Added missing "not" before filesAreEqual... and renamed the file

--- syncurl.nim
+++ syncurl.nim
@@ -23,17 +23,15 @@ proc loadConfig(): Config =
   while true:
     if configFile.readBytes(hashBytes, 0, sizeof(Hash)) < sizeof(Hash): break
     let hash = cast[ptr Hash](addr hashBytes)[]
-    let sl = configFile.readLine().split("|", maxsplit=1)
+    let sl = configFile.readLine().split(">", maxsplit=1)
     #if sl.len != 2: error 
-    let sfi = SynchronizedFileInfo(url:sl[0], localPath:sl[1], localHash:hash)
+    let sfi = SynchronizedFileInfo(url:sl[0].strip(), localPath:sl[1].strip(), localHash:hash)
     result.infos.add(sfi)
-
-proc computeHash(file: File): Hash = file.readAll().hash()
 
 proc computeFileHash(filePath: string): Hash = 
   let file = open(filePath, mode=FileMode.fmRead)
   defer: close file
-  return computeHash(file)
+  return file.readAll().hash()
 
 proc filesAreEqual(filePath1, filePath2: string): bool = 
   if os.getFileSize(filePath1) != os.getFileSize(filePath2): return false 
@@ -52,7 +50,7 @@ while true:
       change = true
       continue
     client.downloadFile(sfi.url, downloadPath)
-    if filesAreEqual(downloadPath, sfi.localPath):
+    if not filesAreEqual(downloadPath, sfi.localPath):
       try:
         echo "updating " & sfi.localPath
         os.removeFile(sfi.localPath)
